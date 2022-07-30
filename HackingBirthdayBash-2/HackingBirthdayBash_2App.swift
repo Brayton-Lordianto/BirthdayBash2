e5f5dd393b7880ea27dcb117499536e15c78ac94
Brayton-Lordianto <braylord30@gmail.com>
2022-07-30 15:01:40 +0800

    Initial Commit

---
+++ HackingBirthdayBash-2/HackingBirthdayBash_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HackingBirthdayBash_2App.swift
+//  HackingBirthdayBash-2
+//
+//  Created by Brayton Lordianto on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingBirthdayBash_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
